cca0f037d7384817ca2671d9479a2fc7b1907a2a
muhammadalihussnain <ali_hussnain@live.com>
2023-11-01 11:25:54 +0500

    hello_bpf.py with the code attached with event execve

---
+++ hello_bpf.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+from bcc import BPF
+
+
+program = """
+int hello(void *ctx){
+bpf_trace_printk("Hello MY eBPF");
+return 0;
+}
+"""
+b   =   BPF(text=program)
+syscall = b.get_syscall_fnname("execve")
+b.attach_kprobe(event=syscall, fn_name='hello')
+b.trace_print()
